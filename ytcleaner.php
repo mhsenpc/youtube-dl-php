79cd53b8923ef08902d3bd84f01802b79908e91c
mohsen <mhsenpc@gmail.com>
2016-11-19 21:49:03 +0330

    Remove old files more than 1 hour

---
+++ ytcleaner.php
@@ -0,0 +1,18 @@
+<?php
+$list = file_get_contents("files.txt");
+$list = json_decode($list);
+$duration = 60 * 60; //1 hour
+foreach($list as $key=>$item){
+	if(time() - $item[1] >= $duration ){
+		$filepath = __DIR__."/files/".$item[0];
+		if(file_exists($filepath))
+			unlink($filepath);
+		
+		
+		echo $item[0]." removed  \n";
+		unset($list[$key]);
+	}	
+}
+
+file_put_contents("files.txt",json_encode($list));
+echo "done";
