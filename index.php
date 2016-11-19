e0fa4cc30abc52b0670b86c99b35b713b20c3087
mohsen <mhsenpc@gmail.com>
2016-11-19 22:22:57 +0330

    add fork me and time limit

--- index.php
+++ index.php
@@ -59,6 +59,9 @@
    </head>
    <body>
       <div class="container">
+<a href="https://github.com/mhsenpc/youtube-dl-php"><img style="position: absolute; top: 0; right: 0; border: 0;" src="https://camo.githubusercontent.com/365986a132ccd6a44c23a9169022c0b5c890c387/68747470733a2f2f73332e616d617a6f6e6177732e636f6d2f6769746875622f726962626f6e732f666f726b6d655f72696768745f7265645f6161303030302e706e67" alt="Fork me on GitHub" data-canonical-src="https://s3.amazonaws.com/github/ribbons/forkme_right_red_aa0000.png"></a>
+
+
          <div class="row" style="height:70px;"></div>
          <div class="row">
             <div class="col-md-4"></div>
@@ -102,13 +105,20 @@
 	<div class="row">&nbsp;</div>
 	<div class="row">&nbsp;</div>
 	<div class="row">&nbsp;</div>
-	
+
+	  <div class="row">
+		<div class="col-md-12">
+		<center>Files will be stored for 24 hours only because of storage limits.</center>
+		</div>
+	  </div>
+	  
 	  <div class="row">
 		<div class="col-md-12">
 		<center>Written by <a href="http://github.com/mhsenpc">mhsenpc</a> 2016</center>
 		</div>
 	  </div>
-	  
-      </div>
+		
+     </div>
+	 
    </body>
 </html>
